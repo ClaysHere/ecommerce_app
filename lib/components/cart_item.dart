146cc144363ae8a7b42ae281cdf98b5412b9ba86
Fikri <muhammadfikri4402@gmail.com>
2024-04-01 14:32:10 +0700

    adding remove item function

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_app/models/cart.dart';
 import 'package:ecommerce_app/models/shoe.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatefulWidget {
   Shoe shoe;
@@ -10,12 +12,21 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
+  // remove item from cart
+  void removeItemFromCart() {
+    Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Image.network(widget.shoe.imagePath),
       title: Text(widget.shoe.name),
       subtitle: Text(widget.shoe.price),
+      trailing: IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: removeItemFromCart,
+      ),
     );
   }
 }
